efc9b923795a60869d4b435b29c19bc10d0f00d2
dwolf1 <dannawolf1@gmail.com>
2016-11-10 16:39:55 -0500

    fixes to posts controller and new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,42 @@ class PostsController < ApplicationController
 
 	def new
 		@custom = Custom.find(current_user.id)
-		@post = Post.new
 	end
 
 	def create
-  		@post = Post.find(params[:id])
+  		@new_post = Post.new(
+      		sleep: params[:post][:sleep],
+      		water: params[:post][:water],
+      		breakfast: params[:post][:breakfast],
+      		lunch: params[:post][:lunch],
+      		dinner: params[:post][:dinner],
+      		tv: params[:post][:tv],
+      		social_media: params[:post][:social_media],
+      		socialization: params[:post][:socialization],
+      		work: params[:post][:work],
+      		content: params[:post][:content],
+      		rating: params[:post][:rating],
+      		indulgence_1: params[:post][:indulgence_1],
+      		indulgence_2: params[:post][:indulgence_2],
+      		indulgence_3: params[:post][:indulgence_3],
+      		indulgence_4: params[:post][:indulgence_4],
+      		indulgence_5: params[:post][:indulgence_5],
+      		responsibility_1: params[:post][:responsibility_1],
+      		responsibility_2: params[:post][:responsibility_2],
+      		responsibility_3: params[:post][:responsibility_3],
+      		responsibility_4: params[:post][:responsibility_4],
+      		responsibility_5: params[:post][:responsibility_5],
+      		activity_1: params[:post][:activity_1],
+      		activity_2: params[:post][:activity_2],
+      		activity_3: params[:post][:activity_3],
+      		activity_4: params[:post][:activity_4],
+      		activity_5: params[:post][:activity_5])
+
+    	if @new_post
+      		redirect_to posts_path
+    	else
+      		redirect_to new_post_path
+    	end
 	end
 
 	def show
@@ -50,8 +81,7 @@ class PostsController < ApplicationController
       		activity_2: params[:post][:activity_2],
       		activity_3: params[:post][:activity_3],
       		activity_4: params[:post][:activity_4],
-      		activity_5: params[:post][:activity_5]
-    	})
+      		activity_5: params[:post][:activity_5]})
 	    if (@post)
 	      redirect_to url_for(:controller => :posts, :action => :show)
 	    else
